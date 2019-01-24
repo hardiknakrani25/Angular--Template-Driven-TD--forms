35ca2b2f45843240b17b90110da6beafb44323c7
Hardik Nakrani <hdnakarani@gmail.com>
2019-01-24 16:19:25 +0530

    Resetting Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,7 +54,7 @@ export class AppComponent {
     this.user.secretQuestion = this.signUpForm.value.secret;
     this.user.answer = this.signUpForm.value.questionAnswer;
     this.user.gender = this.signUpForm.value.gender;
-
+this.signUpForm.reset();
     console.log(this.signUpForm);
   }
 
